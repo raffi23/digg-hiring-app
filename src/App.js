2c89c032ed98e1a9cc86c40a606afab3a28654e5
raffi <rch.services@outlook.com>
2021-04-02 23:44:02 +0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import IntroContainer from "./Pages/Intro/IntroContainer";
 import QuestionaireContainer from "./Pages/Questionaire/QuestionaireContainer";
-import { AnimatedSwitch } from "react-router-transition";
 
 function App() {
   return (
     <Router>
-      <AnimatedSwitch
-        atEnter={{ translateX: 100 }}
-        atLeave={{ translateX: -100 }}
-        atActive={{ translateX: 0 }}
-      >
+      <Switch>
         <Route exact path="/step/2">
           <QuestionaireContainer />
         </Route>
         <Route exact path="/">
           <IntroContainer />
         </Route>
-      </AnimatedSwitch>
+      </Switch>
     </Router>
   );
 }
